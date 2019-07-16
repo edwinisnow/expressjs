20dd33ae1d244609ec0fd962d3861e9e5a98a4d0
edwinisnow <edwin04@gmail.com>
2019-07-17 00:35:19 +0100

    moved the hard coded data into seperate file

---
+++ Members.js
@@ -0,0 +1,23 @@
+// hard coded members
+const members = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  }
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const members = require('./Members');
 
 const app = new express();
 
@@ -8,27 +9,6 @@ const PORT = process.env.PORT || 5000;
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// hard coded members
-const members = [
-  {
-    id: 1,
-    name: 'Leanne Graham',
-    username: 'Bret',
-    email: 'Sincere@april.biz'
-  },
-  {
-    id: 2,
-    name: 'Ervin Howell',
-    username: 'Antonette',
-    email: 'Shanna@melissa.tv'
-  },
-  {
-    id: 3,
-    name: 'Clementine Bauch',
-    username: 'Samantha',
-    email: 'Nathan@yesenia.net'
-  }
-];
 // route
 app.use('/api/members', (req, res) => res.json(members));
 
