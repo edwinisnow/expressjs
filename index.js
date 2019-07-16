2d91755092c2d56923a9541e42f30943bb8bc730
edwinisnow <edwin04@gmail.com>
2019-07-17 00:22:05 +0100

    get members from hardcoded data

--- index.js
+++ index.js
@@ -8,4 +8,28 @@ const PORT = process.env.PORT || 5000;
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// hard coded members
+const members = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  }
+];
+// route
+app.use('/api/members', (req, res) => res.json(members));
+
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
